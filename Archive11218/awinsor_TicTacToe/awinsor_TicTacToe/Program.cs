dbc7f67c9e3e86b4aa1b3650320152996a65df25
winsor09 <202092@student.asd20.org>
2018-11-06 14:37:58 -0700

    TicTacToe finished

--- Archive11218/awinsor_TicTacToe/awinsor_TicTacToe/Program.cs
+++ Archive11218/awinsor_TicTacToe/awinsor_TicTacToe/Program.cs
@@ -10,48 +10,49 @@ namespace awinsor_TicTacToe
     {
         static void Main(string[] args)
         {
-            int x = 0;
-            int y = 0;
-            bool win = false;
+            
             char[,] array = new char[3, 3];
-
-
-            Console.WriteLine("Player 1 it's you are X's ");
-            Console.WriteLine("Player 2 it's you are O's\nDo you understand ? ");
-
-            while (win != true)
+            for (int i = 0; i < 3; i++)
             {
-
-                Console.Clear();
-
-
-                DrawBoard(array);
-
-
-                Console.WriteLine("Player 1 it's your turn, choose a coordinate : ");
-                Console.Write("X Coordinate: ");
-                x = int.Parse(Console.ReadLine());
-                Console.Write("Y Coordinate: ");
-                y = int.Parse(Console.ReadLine());
-
-                ProccessTurn(bool);
-                    array[x, y] = 'X';
-
-
-                Console.Clear();
-                DrawBoard(array);
-
-
-
-                Console.WriteLine("Player 2 it's your turn, choose a coordinate : ");
-                Console.Write("X Coordinate: ");
-                x = int.Parse(Console.ReadLine());
-                Console.Write("Y Coordinate: ");
-                y = int.Parse(Console.ReadLine());
-
-                array[x, y] = 'O';
-
+                for (int j = 0; j < 3; j++)
+                {
+                    array[i, j] = ' ';
+                }
             }
+
+
+                DrawBoard(array);
+
+
+                char result;
+                do
+                {
+                    ProccessTurn(array, 'X');
+                    result = VerifyBoard(array);
+
+                    Console.Clear();
+                    DrawBoard(array);
+
+                    
+                    
+                    //check to see if result is special
+                    
+
+                    ProccessTurn(array, 'O');
+                    result = VerifyBoard(array);
+                    Console.Clear();
+                    DrawBoard(array);
+
+                    
+
+                    //check to see if result is special
+                } while (result == ' ');
+
+                if(result == 'O' || result == 'X')
+                Console.WriteLine("\n\n\n" + result + " WINS !!!");
+
+                if (result == ' ')
+                    Console.WriteLine("\n\n\nNo One Wins.");
 
             Console.ReadLine();
         }
@@ -72,21 +73,86 @@ namespace awinsor_TicTacToe
             }
         }
 
-        public static bool ProccessTurn(char[,] array, int x, int y)
+        public static char VerifyBoard(char[,] array)
         {
-            bool isopen = false;
 
-            if (array[x, y] != 'X' || array[x, y] != 'O')
+
+            //checks for X's
+            if (array[0, 0] == 'X' && array[0, 1] == 'X' && array[0, 2] == 'X')
+            { return 'X'; }
+
+            if (array[1, 0] == 'X' && array[1, 1] == 'X' && array[1, 2] == 'X')
+            { return 'X'; }
+
+            if (array[2, 0] == 'X' && array[2, 1] == 'X' && array[2, 2] == 'X')
+            { return 'X'; }
+
+            if (array[0, 0] == 'X' && array[1, 0] == 'X' && array[2, 0] == 'X')
+            { return 'X'; }
+
+            if (array[0, 1] == 'X' && array[1, 1] == 'X' && array[2, 1] == 'X')
+            { return 'X'; }
+
+            if (array[0, 2] == 'X' && array[1, 2] == 'X' && array[2, 2] == 'X')
+            { return 'X'; }
+
+            if (array[0, 0] == 'X' && array[1, 1] == 'X' && array[2, 2] == 'X')
+            { return 'X'; }
+
+            if (array[2, 0] == 'X' && array[1, 1] == 'X' && array[0, 2] == 'X')
+            { return 'X'; }
+
+
+            //checks for O's
+            if (array[0, 0] == 'O' && array[0, 1] == 'O' && array[0, 2] == 'O')
+            { return 'O'; }
+
+            if (array[1, 0] == 'O' && array[1, 1] == 'O' && array[1, 2] == 'O')
+            { return 'O'; }
+
+            if (array[2, 0] == 'O' && array[2, 1] == 'O' && array[2, 2] == 'O')
+            { return 'O'; }
+
+            if (array[0, 0] == 'O' && array[1, 0] == 'O' && array[2, 0] == 'O')
+            { return 'O'; }
+
+            if (array[0, 1] == 'O' && array[1, 1] == 'O' && array[2, 1] == 'O')
+            { return 'O'; }
+
+            if (array[0, 2] == 'O' && array[1, 2] == 'O' && array[2, 2] == 'O')
+            { return 'O'; }
+
+            if (array[0, 0] == 'O' && array[1, 1] == 'O' && array[2, 2] == 'O')
+            { return 'O'; }
+
+            if (array[2, 0] == 'O' && array[1, 1] == 'O' && array[0, 2] == 'O')
+            { return 'O'; }
+
+           return ' ';
+
+            
+        }
+
+        public static void ProccessTurn(char[,] array, char player)
+        {
+            do
             {
-                isopen = true;
-            }
+                Console.WriteLine(player + " it's your turn, choose a coordinate : ");
+                Console.Write("X Coordinate: ");
+                int x = int.Parse(Console.ReadLine());
+                Console.Write("Y Coordinate: ");
+                int y = int.Parse(Console.ReadLine());
 
-            else
-            {
-                isopen = false;
-            }
 
-            return isopen;
+
+                if (array[x, y] == ' ')
+                {
+                    array[x, y] = player;
+                    break;
+                }
+
+            } while (true);
+
         }
     }
 }
