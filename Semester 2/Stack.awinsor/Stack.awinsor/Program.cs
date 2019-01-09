d3cb720d70f445b219509d75cd8496acdf7d3460
winsor09 <202092@student.asd20.org>
2019-01-09 14:41:44 -0700

    STACK

---
+++ Semester 2/Stack.awinsor/Stack.awinsor/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.awinsor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 0;
+            int menu = 0;
+            stack mystack = new stack();
+
+
+            while (menu != 5)
+            {
+                Console.WriteLine("1 - Pop");
+                Console.WriteLine("2 - Push");
+                Console.WriteLine("3 - Peek");
+                Console.WriteLine("4 - Print");
+                Console.WriteLine("5 - End");
+
+                Console.Write("\n\nPlease Choose an Option from the menu above: ");
+
+                menu = int.Parse(Console.ReadLine());
+
+
+
+                switch (menu)
+                {
+                    case 1:
+
+                        Console.WriteLine(mystack.Pop());
+                        Console.ReadLine();
+
+                        break;
+
+                    case 2:
+
+                        Console.Write("Please choose a number to push to the stack: ");
+                        val = int.Parse(Console.ReadLine());
+                        mystack.Push(val);
+
+                        Console.ReadLine();
+
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine(mystack.Peek());
+
+                        Console.ReadLine();
+
+                        break;
+
+                    case 4:
+
+                        mystack.Print();
+
+                        Console.ReadLine();
+
+                        break;
+                }
+
+                
+            }
+            
+        }
+    }
+}
---
+++ Semester 2/Stack.awinsor/Stack.awinsor/stack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.awinsor
+{
+    class stack
+    {
+        
+        List<int> holder = new List<int>();
+
+        public stack()
+        {
+
+        }
+
+        //push
+        public void Push(int val)
+        {
+            
+
+            holder.Add(val);
+        }
+
+        //pop
+        public int Pop()
+        {
+            
+            if (holder.Count > 1)
+            {
+                int dumdum = holder[holder.Count - 1];
+
+                holder.RemoveAt(holder.Count - 1);
+
+                return dumdum;
+            }
+            else
+            return 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+            
+        }
+
+        //peek
+        public int Peek()
+        {
+            if (holder.Count > 1)
+            {
+                int dumdum = holder[holder.Count - 1];
+
+                return dumdum;
+            }
+
+            else
+                return 0000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+        }
+
+        //print
+        public void Print()
+        {
+            for (int i = 0; i < holder.Count; i++)
+            {
+                Console.WriteLine(holder[i]);
+            }
+        }
+    }
+}
