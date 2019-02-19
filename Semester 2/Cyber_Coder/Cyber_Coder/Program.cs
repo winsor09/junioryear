40e42bb5cf4ed7eaf5a4914fd202a661768c156b
winsor09 <202092@student.asd20.org>
2019-02-19 13:27:30 -0700

    LINQ Palindrome

---
+++ Semester 2/Cyber_Coder/Cyber_Coder/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cyber_Coder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //reading in the file
+            string text = AppDomain.CurrentDomain.BaseDirectory + @"Prob01.in.txt";
+
+            
+            List<List<string>> file = new List<List<string>>();
+            
+            //puts file into list so it can be read
+            using (StreamReader sr = new StreamReader(text))
+            {
+
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    file.Add(new List<string>());
+                    string[] Array = line.Split(',');
+                    for (int i = 0; i < Array.Length; i++)
+                    {
+                        file[file.Count - 1].Add(Array[i]);
+                    }
+
+                }
+            }
+            
+            for (int i = 0; i < file.Count; i++)
+            {
+
+                
+
+                file[i] = file[i].OrderBy(t => t).ToList();
+            }
+
+
+            for (int i = 0; i < file.Count; i++)
+            {
+                for (int j = 0; j < file[i].Count; j++)
+                {
+                    Console.Write(file[i][j] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Semester 2/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plndrm = "";
+
+            Console.WriteLine("Please type a sentence out so it can be checked if it's a palindrome.");
+            plndrm = Console.ReadLine();
+
+            plndrm = plndrm.Replace(" ", "").Replace(".", "").Replace("'", "").Replace(",", "").Replace(";", "").Replace("!", "").Replace("?", "").Replace("\"", "").Replace(":", "").Replace("(", "").Replace(")", "").Replace("%", "").Replace("-", "").Replace("/", "").Replace("$", "");
+            plndrm = plndrm.ToLower();
+
+            //for(int i = 0; i < plndrm.Length; i++)
+            //{
+            //    if (plndrm[i] != plndrm[plndrm.Length - 1 - i ])
+            //    {
+            //        i = plndrm.Length - 1;
+            //        Console.WriteLine("This is not a palindrome.");
+            //    }
+
+            //    else if (plndrm[i] == plndrm[plndrm.Length - 1 - i])
+            //    {
+            //        if (i == plndrm.Length - 1)
+            //        {
+            //            Console.WriteLine("This is infact a palindrome.");
+            //        }
+
+            //    }
+
+
+            //}
+
+            string reverse = new string(plndrm.Reverse().ToArray());
+
+            if (reverse == plndrm)
+            {
+                Console.WriteLine("This is infact a palindrome.");
+            }
+
+
+            if (reverse != plndrm)
+            {
+                Console.WriteLine("This is infact not a palindrome.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
